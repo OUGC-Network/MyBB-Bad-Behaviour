6b19f19990b1d382346a727d85b17662ef5e7665
Omar Gonzalez <Sama34@users.noreply.github.com>
2025-11-19 23:07:08 -0800

    PSR12

--- upload/inc/languages/english/admin/badbehavior.lang.php
+++ upload/inc/languages/english/admin/badbehavior.lang.php
@@ -1,26 +1,26 @@
 <?php
 
 /**
-* @package	Bad Behavior
-* @author	Eric Sizemore <admin@secondversion.com>
-* @version	1.0.1
-* @license	GNU LGPL http://www.gnu.org/licenses/lgpl.txt
-* 
-*	Bad Behavior - Integrates MyBB and Bad Behavior
-*	Copyright (C) 2011 - 2014 Eric Sizemore
-*
-*	Bad Behavior is free software; you can redistribute it and/or modify it under
-*	the terms of the GNU Lesser General Public License as published by the Free
-*	Software Foundation; either version 3 of the License, or (at your option) any
-*	later version.
-*
-*	This program is distributed in the hope that it will be useful, but WITHOUT ANY
-*	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-*	PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-*
-*	You should have received a copy of the GNU Lesser General Public License along
-*	with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * @package    Bad Behavior
+ * @author    Eric Sizemore <admin@secondversion.com>
+ * @version    1.0.1
+ * @license    GNU LGPL http://www.gnu.org/licenses/lgpl.txt
+ *
+ *    Bad Behavior - Integrates MyBB and Bad Behavior
+ *    Copyright (C) 2011 - 2014 Eric Sizemore
+ *
+ *    Bad Behavior is free software; you can redistribute it and/or modify it under
+ *    the terms of the GNU Lesser General Public License as published by the Free
+ *    Software Foundation; either version 3 of the License, or (at your option) any
+ *    later version.
+ *
+ *    This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *    PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License along
+ *    with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
 Bad Behavior - detects and blocks unwanted Web accesses
@@ -42,32 +42,32 @@ Please report any problems to bad . bots AT ioerror DOT us
 http://www.bad-behavior.ioerror.us/
 */
 
-$l['badbehavior_logs_index'] = 'Bad Behavior Logs';
-$l['badbehavior_logs_canmanage'] = 'Can manage Bad Behavior Logs?';
-$l['badbehavior_logs'] = 'Bad Behavior Log';
-$l['badbehavior_logs_desc'] = 'View bad behavior logs.';
-$l['badbehavior_logs_prune'] = 'Prune Log';
-$l['badbehavior_logs_prune_days'] = 'Older than';
-$l['badbehavior_logs_prune_days_desc'] = 'Prune log entries older than the number of days you enter.';
-$l['badbehavior_logs_ipaddress'] = 'IP Address';
-$l['badbehavior_logs_key'] = 'Key';
-$l['badbehavior_logs_useragent'] = 'User Agent';
-$l['badbehavior_logs_request_method'] = 'Method';
-$l['badbehavior_logs_server_protocol'] = 'Protocol';
-$l['badbehavior_logs_request_uri'] = 'URI';
-$l['badbehavior_logs_request_entity'] = 'Entity';
-$l['badbehavior_logs_http_headers'] = 'Headers';
-$l['badbehavior_logs_date'] = 'Date';
-$l['badbehavior_logs_options'] = 'Options';
-$l['badbehavior_logs_delete'] = 'Delete';
-$l['badbehavior_logs_none'] = 'No logs found.';
-$l['badbehavior_logs_error'] = 'An unknown error has occurred.';
-$l['badbehavior_logs_invalid'] = 'Invalid log entry.';
-$l['badbehavior_logs_deleted'] = 'Log entry deleted successfully.';
-$l['badbehavior_logs_pruned']= 'Log pruned successfully.';
-$l['badbehavior_logs_pruneconfirm'] = 'Are you sure you want to prune the log?';
-$l['badbehavior_logs_deleteconfirm'] = 'Are you sure you want to delete the selected log entry?';
-$l['badbehavior_logs_submit'] = 'Submit';
-$l['badbehavior_logs_reset'] = 'Reset';
-
-?>
+$l = [
+    'badbehavior_logs_index' => 'Bad Behavior Logs',
+    'badbehavior_logs_canmanage' => 'Can manage Bad Behavior Logs?',
+    'badbehavior_logs' => 'Bad Behavior Log',
+    'badbehavior_logs_desc' => 'View bad behavior logs.',
+    'badbehavior_logs_prune' => 'Prune Log',
+    'badbehavior_logs_prune_days' => 'Older than',
+    'badbehavior_logs_prune_days_desc' => 'Prune log entries older than the number of days you enter.',
+    'badbehavior_logs_ipaddress' => 'IP Address',
+    'badbehavior_logs_key' => 'Key',
+    'badbehavior_logs_useragent' => 'User Agent',
+    'badbehavior_logs_request_method' => 'Method',
+    'badbehavior_logs_server_protocol' => 'Protocol',
+    'badbehavior_logs_request_uri' => 'URI',
+    'badbehavior_logs_request_entity' => 'Entity',
+    'badbehavior_logs_http_headers' => 'Headers',
+    'badbehavior_logs_date' => 'Date',
+    'badbehavior_logs_options' => 'Options',
+    'badbehavior_logs_delete' => 'Delete',
+    'badbehavior_logs_none' => 'No logs found.',
+    'badbehavior_logs_error' => 'An unknown error has occurred.',
+    'badbehavior_logs_invalid' => 'Invalid log entry.',
+    'badbehavior_logs_deleted' => 'Log entry deleted successfully.',
+    'badbehavior_logs_pruned' => 'Log pruned successfully.',
+    'badbehavior_logs_pruneconfirm' => 'Are you sure you want to prune the log?',
+    'badbehavior_logs_deleteconfirm' => 'Are you sure you want to delete the selected log entry?',
+    'badbehavior_logs_submit' => 'Submit',
+    'badbehavior_logs_reset' => 'Reset',
+];
